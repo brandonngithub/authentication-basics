451568d66f6d4d4f7008e28026b8782638ef5964
Brandon Ngo <brandonngo56@gmail.com>
2025-01-01 17:52:33 -0500

    Create login form

--- app.js
+++ app.js
@@ -6,13 +6,11 @@ const passport = require("passport");
 const LocalStrategy = require('passport-local').Strategy;
 const dotenv = require('dotenv');
 
+const app = express();
 dotenv.config();
-
 const pool = new Pool({
     connectionString: process.env.PSQL_CONNECTION_STRING
 });
-
-const app = express();
 
 passport.use(
     new LocalStrategy(async (username, password, done) => {
@@ -53,8 +51,12 @@ app.set("view engine", "ejs");
 app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    next();
+});  
 
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => res.render("index"));  
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
 app.post("/sign-up", async (req, res, next) => {
     try {
@@ -67,5 +69,18 @@ app.post("/sign-up", async (req, res, next) => {
         return next(err);
     }
 });
-  
+app.post("/log-in", passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/"
+    })
+);
+app.get("/log-out", (req, res, next) => {
+    req.logout((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
+});  
+
 app.listen(3000, () => console.log("app listening on port 3000!"));
