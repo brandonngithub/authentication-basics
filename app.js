879a55dbcd396b8584308af8f07843a41c69d711
Brandon Ngo <brandonngo56@gmail.com>
2025-01-01 17:17:16 -0500

    Create passport functions

--- app.js
+++ app.js
@@ -13,6 +13,40 @@ const pool = new Pool({
 });
 
 const app = express();
+
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+            const user = rows[0];
+    
+            if (!user) {
+                return done(null, false, { message: "Incorrect username" });
+            }
+            if (user.password !== password) {
+                return done(null, false, { message: "Incorrect password" });
+            }
+            return done(null, user);
+        } catch(err) {
+            return done(err);
+        }
+    })
+);
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+  
+passport.deserializeUser(async (id, done) => {
+    try {
+        const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+        const user = rows[0];
+        done(null, user);
+    } catch(err) {
+        done(err);
+    }
+});
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
